9a77392f28ef37f9b614c4a6100ee4a4229f86aa
gian <michaelgiantiqui3@gmail.com>
2024-01-17 20:46:08 +0800

    saasddas

---
+++ components/Meow.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Meow = () => {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+};
+
+export default Meow;
