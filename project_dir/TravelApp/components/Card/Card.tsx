bf28dcb42bc128e879195728e04dffe2b2c5dafb
gian <michaelgiantiqui3@gmail.com>
2024-01-27 09:06:15 +0800

    test

--- project_dir/TravelApp/components/Card/Card.tsx
+++ project_dir/TravelApp/components/Card/Card.tsx
@@ -1,19 +1,53 @@
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+type SampleData = {
+  id: string;
+  uri: any;
+  text: string;
+  text2: string;
+  hearts: boolean;
+};
 
 type RenderItemProps = {
+  id: string;
   uri: string;
   text: string;
   text2: string;
+  handleHeartPressed: any;
+  data: SampleData[];
 };
 
-const Card = ({uri, text, text2}: RenderItemProps) => {
+const Card = ({
+  id,
+  uri,
+  text,
+  text2,
+  handleHeartPressed,
+  data,
+}: RenderItemProps) => {
   return (
     <View style={styles.cardContainer}>
       <ImageBackground source={{uri: uri}} style={styles.cardImage}>
         <View style={styles.cardFooter}>
           <Text style={styles.overlayText1}>{text}</Text>
           <Text style={styles.overlayText2}>{text2}</Text>
+          <TouchableOpacity
+            style={styles.circle}
+            onPress={() => handleHeartPressed(id)}>
+            {data.find(d => d.id === id)?.hearts ? (
+              <AntDesign name="heart" color={'red'} size={22} />
+            ) : (
+              <AntDesign name="hearto" color={'grey'} size={22} />
+            )}
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
@@ -22,16 +56,16 @@ const Card = ({uri, text, text2}: RenderItemProps) => {
 
 const styles = StyleSheet.create({
   cardContainer: {
-    width: 220,
-    height: 270,
+    width: 180,
+    height: 210,
     borderRadius: 20,
     overflow: 'hidden',
     marginRight: 20,
-    elevation: 5,
+    elevation: 3,
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 5},
     shadowOpacity: 0.5,
-    shadowRadius: 4,
+    shadowRadius: 1,
   },
   cardImage: {
     width: '100%',
@@ -54,13 +88,44 @@ const styles = StyleSheet.create({
   },
   overlayText1: {
     color: '#000',
-    fontSize: 21,
+    fontSize: 16,
     fontWeight: 'bold',
   },
   overlayText2: {
     color: '#000',
-    fontSize: 17,
+    fontSize: 12,
     fontWeight: 'bold',
+  },
+  circle: {
+    position: 'absolute',
+    right: 15,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    padding: 7,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 23,
+    borderColor: '#000',
+    borderWidth: 0.2,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+  circleRed: {
+    position: 'absolute',
+    right: 20,
+    top: 23,
+    backgroundColor: 'red',
+    borderRadius: 50,
+    padding: 7,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
   },
 });
 
