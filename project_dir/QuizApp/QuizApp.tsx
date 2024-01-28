b8f90e4a8666648f958540b9281659fb3d3808e9
gian <michaelgiantiqui3@gmail.com>
2024-01-28 12:39:14 +0800

    Quiz App Finished

--- project_dir/QuizApp/QuizApp.tsx
+++ project_dir/QuizApp/QuizApp.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Index from './navigation/Index';
 
+/*
+
+    QUIZ APP MAIN CONTAINER
+
+*/
+
 const QuizApp = () => {
   return (
     <NavigationContainer>
